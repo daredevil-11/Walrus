acadcf1c1a09ec8e98a2efc4083c710aa9a5e423
Tanmay Das <tanmay-das@hotmail.com>
2021-10-16 12:21:55 +0530

    create the rouge api

---
+++ Walrus.Rouge.Api/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace Walrus.Rouge.Api
+{
+    public class Constants
+    {
+        public const string _failureType = "Walrus:RougeApi:FailureType";
+        public const string _failureCount = "Walrus:RougeApi:FailureCount";
+        public enum FailureTypes
+        {
+            ShortBursts = 1,
+            LongTime
+        }
+    }
+}
---
+++ Walrus.Rouge.Api/Controllers/RougeController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Threading;
+
+namespace Walrus.Rouge.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class RougeController : ControllerBase
+    {
+        private const int MAX_FAILURE_COUNT = 3;
+        private readonly IMemoryCache _memoryCache;
+
+        public RougeController(IMemoryCache memoryCache) 
+        {
+            _memoryCache = memoryCache ?? throw new ArgumentNullException(nameof(memoryCache));
+        }
+
+        [HttpGet]
+        public ObjectResult Get()
+        {
+            _memoryCache.TryGetValue(Constants._failureType, out string failureTypeStr);
+            if(failureTypeStr == default || string.IsNullOrWhiteSpace(failureTypeStr) 
+                || !Enum.TryParse(failureTypeStr, out Constants.FailureTypes failureType))
+            {
+                failureType = (Constants.FailureTypes)(DateTimeOffset.Now.Millisecond % 2 + 1);
+                _memoryCache.Set(Constants._failureType, failureType.ToString(), new MemoryCacheEntryOptions()
+                {
+                    AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5)
+                });
+                _memoryCache.Set(Constants._failureCount, 0);
+            }
+
+            _memoryCache.TryGetValue(Constants._failureCount, out int failureCount);
+            if(failureCount == default || failureCount < 0)
+            {
+                failureCount = 0;
+                _memoryCache.Set(Constants._failureCount, failureCount);
+            }
+
+            // short bust failures
+            if(failureType == Constants.FailureTypes.ShortBursts)
+            {
+                if(++failureCount <= MAX_FAILURE_COUNT)
+                {
+                    _memoryCache.Set(Constants._failureCount, failureCount);
+                    return new ObjectResult(null)
+                    {
+                        StatusCode = 503,
+                    };
+                }
+                else
+                {
+                    _memoryCache.Set(Constants._failureCount, 0);
+                    return new ObjectResult(true)
+                    {
+                        StatusCode = 200,
+                    };
+                }
+            }
+            // long noticable failure
+            else
+            {
+                if (++failureCount <= MAX_FAILURE_COUNT)
+                {
+                    Thread.Sleep(TimeSpan.FromMinutes(1));
+                    _memoryCache.Set(Constants._failureCount, failureCount);
+                    return new ObjectResult(null)
+                    {
+                        StatusCode = 504,
+                    };
+                }
+                else
+                {
+                    _memoryCache.Set(Constants._failureCount, 0);
+                    return new ObjectResult(true)
+                    {
+                        StatusCode = 200,
+                    };
+                }
+            }
+        }
+
+        [HttpGet]
+        public ObjectResult Test()
+        {
+            return new OkObjectResult("OK");
+        }
+    }
+}
